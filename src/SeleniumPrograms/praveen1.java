810c168acb30126a820bda2a8ada177f6113a624
prave <prave@PRAVEEN>
2020-12-26 09:34:05 +0530

    Praveen manda added 2 scripts under selenium package of sprint 10

---
+++ src/SeleniumPrograms/praveen1.java
@@ -0,0 +1,10 @@
+package SeleniumPrograms;
+
+public class praveen1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/SeleniumPrograms/praveen2.java
@@ -0,0 +1,10 @@
+package SeleniumPrograms;
+
+public class praveen2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
